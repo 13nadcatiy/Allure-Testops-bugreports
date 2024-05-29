8bdd46d6b7bf26f5b14fcbf0e384917df70ec760
13th <pavel.voronin@ligastavok.ru>
2024-05-29 17:42:35 +0300

    Добавлен нестабильный тест

--- src/test/java/dev/pivozavr/bugreports/testops/ExampleTestClass.java
+++ src/test/java/dev/pivozavr/bugreports/testops/ExampleTestClass.java
@@ -10,17 +10,16 @@ import static io.qameta.allure.Allure.step;
 
 class ExampleTestClass {
 
-    static int tryCount = 0;
+    static int variationCount = 3;
 
     @Tag("voronin-test")
     @DisplayName("Параметризированный тест (заканчивается успешно только в последней итерации)")
     @ParameterizedTest(name = "{displayName} ({argumentsWithNames})")
     @ValueSource(strings = {"test1", "test2", "test3"})
     public void exampleTest_1_v1(String arg) {
-        tryCount++;
-
+        variationCount--;
         step("Проверка", () -> {
-            Assertions.assertEquals(3, tryCount, "Тест пройдет успешно только в последней итерации");
+            Assertions.assertEquals(0, variationCount, "Тест должен пройти  успешно только на 3й раз");
         });
     }
 }
