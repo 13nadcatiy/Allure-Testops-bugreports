1da156933faa5b1298da6bd4724ee7da66752563
13th <pavel.voronin@ligastavok.ru>
2024-05-29 15:55:12 +0300

    Added unstable param test

--- src/test/java/dev/pivozavrus/bugreports/testops/ExampleTestClass.java
+++ src/test/java/dev/pivozavrus/bugreports/testops/ExampleTestClass.java
@@ -1,25 +1,27 @@
 package dev.pivozavrus.bugreports.testops;
 
-import io.qameta.allure.AllureId;
-import io.qameta.allure.Step;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.qameta.allure.Allure.step;
 
 class ExampleTestClass {
 
+    static int tryCount = 0;
+
     @Tag("voronin-test")
-    @DisplayName("Параметризированный тест")
+    @DisplayName("Параметризированный тест (заканчивается успешно только в последней итерации)")
     @ParameterizedTest(name = "{displayName} ({argumentsWithNames})")
     @ValueSource(strings = {"test1", "test2", "test3"})
     public void exampleTest_1_v1(String arg) {
-        step("Шаг теста", () -> {
-            //Происходит магия
+        tryCount++;
+
+        step("Проверка", () -> {
+            Assertions.assertEquals(3, tryCount, "Тест пройдет успешно только в последней итерации");
         });
     }
 
